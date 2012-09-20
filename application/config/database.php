3be37ae0984c815164dcfbe90721a0e8f8500fc6
reginaldosc <reginaldo.goncalves.sc@gmail.com>
2012-09-20 18:23:12 -0300

    Teste bd

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = '127.0.0.1';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'intelbras';
 $db['default']['database'] = 'rg_quality';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
