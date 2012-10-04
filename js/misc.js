f632315abc5de802f3497c282b83d13b95d1d768
Jairo Jair <jairojair@gmail.com>
2012-10-04 11:00:07 -0300

    Melhorias do controle de modal via Javascript

--- js/misc.js
+++ js/misc.js
@@ -7,11 +7,44 @@ $(document).ready(function(){
 		var comment = $('#Comentario').val();
 		var auditoria = $('#Auditoria').val();
 
+		// Limpa //
+		$('#Descricao').val('');
+		$('#Data').val('');
+		$('#Comentario').val('');
+
 		$.post("../../nc/cadastrarNc/", { Descricao : desc , Data : date, Comentario : comment, Auditoria : auditoria }, "json");
 
-		$("#NCmodal").fadeOut("slow");	
+
+		$('.modal-body').slideUp(400);
+
+		$('#alert-msg').slideDown(400).delay(2000);
+		$('#alert-msg').slideUp(400);
+
+		// Implementar reset modal //
 	
 	});
+
+	// Analisa o radio selecionado //
+	$(':radio').click ( function()
+	{
+		if ($(this).is(':checked') && $(this).val() == 6) 
+		{
+			$('#NCmodal').modal('show');
+		}
+
+	});
+
+
+	// Executa quando o modal for fechado //
+	$('#NCmodal').on('hidden', function () {
+		  
+		// Limpa //
+		$('#Descricao').val('');
+		$('#Data').val('');
+		$('#Comentario').val('');
+
+	})
+
 
 
 	// Datas para o sistema //
